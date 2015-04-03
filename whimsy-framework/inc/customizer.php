1991453c87b0cbf3cc59b10806ff85f7ddda2514
natashakcross <natasha@Natashas-MacBook-Pro.local>
2015-04-03 17:08:56 -0500

    Major changes
    Added support for mobile and desktop logos, and navigation display
    settings.

--- whimsy-framework/inc/customizer.php
+++ whimsy-framework/inc/customizer.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * whimsy Theme Customizer
+ * Whimsy Framework Theme Customizer
  *
  * @package whimsy
  */
@@ -8,16 +8,38 @@
 /**
  * Adds the individual sections, settings, and controls to the theme customizer
  */
+
 function whimsy_customize_register_section( $wp_customize ) {	
 
-	/* Add Skin Settings */
+	/* Add Logo Settings Section */
+    $wp_customize->add_section(
+        'whimsy_framework_section_logo',
+        array(
+            'title' 		=> __( 'Logo', 'whimsy-framework' ),
+            'description' 	=> __( 'Add a logo for desktop and mobile screens.', 'whimsy-framework' ),
+            'priority' 		=> 35,
 
+        )
+    );
+
+	/* Add Menu Settings Section */
+    $wp_customize->add_section(
+        'whimsy_framework_section_menu',
+        array(
+            'title' 		=> __( 'Navigation', 'whimsy-framework' ),
+            'description' 	=> __( 'Adjust the way your navigation menu looks.', 'whimsy-framework' ),
+            'priority' 		=> 36,
+
+        )
+    );
+
+    /* Add Skin Settings */
     $wp_customize->add_section(
         'whimsy_framework_section_display',
         array(
-            'title' 		=> __( 'Content Display', 'whimsy-framework' ),
-            'description' 	=> __( 'Choose where the content and sidebar should be displayed.', 'whimsy-framework' ),
-            'priority' 		=> 35,
+            'title'         => __( 'Layout', 'whimsy-framework' ),
+            'description'   => __( 'Choose where the content and sidebar should be displayed.', 'whimsy-framework' ),
+            'priority'      => 37,
 
         )
     );
@@ -33,6 +55,9 @@ function whimsy_customize_register( $wp_customize ) {
 	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
 	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
 	$wp_customize->get_setting( 'header_textcolor')->transport  = 'postMessage';
+
+// Add Color Support
+
 	$wp_customize->add_setting(
 	    'whimsy_link_color',
 	    array(
@@ -51,6 +76,7 @@ function whimsy_customize_register( $wp_customize ) {
 	        )
 	    )
 	);
+
 	$wp_customize->add_setting(
 	    'whimsy_alt_color',
 	    array(
@@ -69,6 +95,7 @@ function whimsy_customize_register( $wp_customize ) {
 	        )
 	    )
 	);
+
 	$wp_customize->add_setting(
 	    'whimsy_body_color',
 	    array(
@@ -87,7 +114,9 @@ function whimsy_customize_register( $wp_customize ) {
 	        )
 	    )
 	);
-	/* Add Layout Settings */
+
+// Add Layout Settings
+
     $wp_customize->add_setting(
         'whimsy_framework_layout',
         array(
@@ -109,8 +138,148 @@ function whimsy_customize_register( $wp_customize ) {
             ),
         )
     );
+
+// Add Logo Settings 
+
+    // Desktop logo
+    $wp_customize->add_setting(
+        'whimsy_framework_logo_desktop',
+        array(
+            'default' => '',
+            'sanitize_callback' => 'esc_url_raw',
+        )
+    );
+    $wp_customize->add_control( 
+    	new WP_Customize_Image_Control( 
+    		$wp_customize, 'whimsy_framework_logo_desktop', 
+    		array(
+    		'label'    => __( 'Desktop Logo', 'whimsy-framework' ),
+            'description'   => __( 'Displayed on most screens.', 'whimsy-framework' ),    		
+            'section'  => 'whimsy_framework_section_logo',
+    		'settings' => 'whimsy_framework_logo_desktop',
+    ) ) );
+
+    // Mobile logo
+    $wp_customize->add_setting(
+        'whimsy_framework_logo_mobile',
+        array(
+            'default' => '',
+            'sanitize_callback' => 'esc_url_raw',
+        )
+    );
+    $wp_customize->add_control( 
+        new WP_Customize_Image_Control( 
+            $wp_customize, 'whimsy_framework_logo_mobile', 
+            array(
+            'label'    => __( 'Mobile Logo', 'whimsy-framework' ),
+            'description'   => __( 'Displayed on screens less than 980px.', 'whimsy-framework' ),
+            'section'  => 'whimsy_framework_section_logo',
+            'settings' => 'whimsy_framework_logo_mobile',
+    ) ) );
+
+    // Centered logo checkbox
+    $wp_customize->add_setting(
+        'whimsy_framework_logo_center',
+        array(
+            'default' => true,
+            'sanitize_callback' => 'sanitize_whimsy_framework_checkbox',
+        )
+    );
+    $wp_customize->add_control(
+        'whimsy_framework_logo_center',
+        array(
+            'type' => 'checkbox',
+            'label' => __( 'Center desktop logo on the page.', 'whimsy-framework' ),
+            'section' => 'whimsy_framework_section_logo',
+        )
+    );
+// Add Menu Display Settings
+
+    // Menu bg color
+    $wp_customize->add_setting(
+        'whimsy_menu_background_color',
+        array(
+            'default' => '#ffffff',
+            'sanitize_callback' => 'sanitize_hex_color',
+        )
+    );
+    $wp_customize->add_control(
+        new WP_Customize_Color_Control(
+            $wp_customize,
+            'whimsy_menu_background_color',
+            array(
+                'label'     => __( 'Background Color', 'whimsy-framework' ),
+                'section'   => 'whimsy_framework_section_menu',
+                'settings'  => 'whimsy_menu_background_color',
+            )
+        )
+    );	
+
+    // Menu link color
+    $wp_customize->add_setting(
+        'whimsy_menu_link_color',
+        array(
+            'default' => '#18a2b6',
+            'sanitize_callback' => 'sanitize_hex_color',
+        )
+    );
+    $wp_customize->add_control(
+        new WP_Customize_Color_Control(
+            $wp_customize,
+            'whimsy_menu_link_color',
+            array(
+                'label'     => __( 'Menu Link Color', 'whimsy-framework' ),
+                'section'   => 'whimsy_framework_section_menu',
+                'settings'  => 'whimsy_menu_link_color',
+            )
+        )
+    );  
+
+    // Submenu bg color
+    $wp_customize->add_setting(
+        'whimsy_submenu_background_color',
+        array(
+            'default' => '#ffffff',
+            'sanitize_callback' => 'sanitize_hex_color',
+        )
+    );
+    $wp_customize->add_control(
+        new WP_Customize_Color_Control(
+            $wp_customize,
+            'whimsy_submenu_background_color',
+            array(
+                'label'     => __( 'Sub-Menu Background Color', 'whimsy-framework' ),
+                'section'   => 'whimsy_framework_section_menu',
+                'settings'  => 'whimsy_submenu_background_color',
+            )
+        )
+    );  
+
+    // Submenu link color
+    $wp_customize->add_setting(
+        'whimsy_submenu_link_color',
+        array(
+            'default' => '#18a2b6',
+            'sanitize_callback' => 'sanitize_hex_color',
+        )
+    );
+    $wp_customize->add_control(
+        new WP_Customize_Color_Control(
+            $wp_customize,
+            'whimsy_submenu_link_color',
+            array(
+                'label'     => __( 'Sub-Menu Link Color', 'whimsy-framework' ),
+                'section'   => 'whimsy_framework_section_menu',
+                'settings'  => 'whimsy_submenu_link_color',
+            )
+        )
+    ); 
+
+// End menu settings
 }
 add_action( 'customize_register', 'whimsy_customize_register' );
+
+// Sanitize layout setting
 
 function sanitize_whimsy_framework_layout( $input ) {
     $valid = array(
@@ -121,14 +290,26 @@ function sanitize_whimsy_framework_layout( $input ) {
  
     if ( array_key_exists( $input, $valid ) ) {
         return $input;
+    } 
+
+    else {
+        return '';
+    }
+}
+
+// Sanitize checkboxes
+function sanitize_whimsy_framework_checkbox( $input ) {
+    if ( $input == 1 ) {
+        return 1;
     } else {
         return '';
     }
 }
+
 /**
  * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
  */
 function whimsy_customize_preview_js() {
-	wp_enqueue_script( 'whimsy_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '1.0', true );
+    wp_enqueue_script( 'whimsy_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '1.0', true );
 }
 add_action( 'customize_preview_init', 'whimsy_customize_preview_js' );
