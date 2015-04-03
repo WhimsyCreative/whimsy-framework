ccf505a647f3f116a7eab4c4740829e020fa408c
natashakcross <natasha@Natashas-MacBook-Pro.local>
2015-04-03 17:11:09 -0500

    Added image upload support

--- whimsy-framework/inc/widgets/widget-about-me.php
+++ whimsy-framework/inc/widgets/widget-about-me.php
@@ -6,31 +6,60 @@ class Widget_Whimsy_About extends WP_Widget {
 
     // Create Widget
     function __construct() {
+
         parent::__construct(
             'widget_whimsy_about', // Base ID
             __( '(Whimsy) About Me', 'whimsy-framework' ), // Name
             array( 'description' => __( 'Your photo, a blurb about yourself, and a link for more info.', 'whimsy-framework' ), ) // Args
         );
+
+        add_action('admin_enqueue_scripts', array($this, 'upload_scripts'));
+        add_action('admin_enqueue_styles', array($this, 'upload_styles'));
     }
+
+    /**
+     * Upload the Javascripts for the media uploader
+     */
+    public function upload_scripts()
+    {
+        wp_enqueue_script('media-upload');
+        wp_enqueue_script('thickbox');
+        wp_enqueue_script('upload_media_widget', get_template_directory_uri() . '/js/upload-media.js', array('jquery'));
+    }
+
+    /**
+     * Add the styles for the upload media box
+     */
+    public function upload_styles()
+    {
+        wp_enqueue_style('thickbox');
+    }
+
     // Widget Content
     public function widget( $args, $instance ) {
 
         echo $args[ 'before_widget' ];
-    
-        $whimsy_title       = apply_filters( 'widget_title', $instance[ 'whimsy_title' ]);
-        $whimsy_image_url   = empty( $instance[ 'whimsy_image_url' ] ) ? '' : $instance[ 'whimsy_image_url' ];
+                  
         $whimsy_text        = empty( $instance[ 'whimsy_text' ] ) ? '' : $instance[ 'whimsy_text' ];
+      
         $whimsy_more_link   = empty( $instance[ 'whimsy_more_link' ] ) ? '' : $instance[ 'whimsy_more_link' ];
+       
         $whimsy_more        = empty( $instance[ 'whimsy_more' ] ) ? '' : $instance[ 'whimsy_more' ];
-        
-        if ( ! empty( $instance['whimsy_title'] ) ) {
-            echo $args['before_title'] . apply_filters( 'widget_title', $instance['whimsy_title'] ). $args['after_title'];
-        }
-        ?>
 
-                <?php if( !empty( $whimsy_image_url ) ) : ?>
+        $whimsy_image = '';
+            if(isset($instance['whimsy_image']))
+            {
+                $whimsy_image = $instance['whimsy_image'];
+            }
+        
+        if ( ! empty( $instance['whimsy_about_title'] ) ) {
+            echo $args['before_title'] . apply_filters( 'widget_title', $instance['whimsy_about_title'] ). $args['after_title'];
+        }
+
+        ?>
+                <?php if( !empty( $whimsy_image ) ) : ?>
                 <div class="whimsy-img">
-                    <a href="<?php echo esc_url_raw( $whimsy_more_link ); ?>"><img src="<?php echo esc_url_raw( $whimsy_image_url ); ?>"></a>
+                    <a href="<?php echo esc_url_raw( $whimsy_more_link ); ?>"><img src="<?php echo esc_url_raw( $whimsy_image ); ?>"></a>
                 </div> <!-- .whimsy-img -->
                 <?php endif; ?>
                 <?php if( !empty( $whimsy_text ) ) : ?>
@@ -53,21 +82,33 @@ class Widget_Whimsy_About extends WP_Widget {
         // Form for widget content
         public function form( $instance ) {
 
-            $whimsy_title       = ! empty( $instance[ 'whimsy_title' ] ) ? $instance[ 'whimsy_title' ] : '';
-            $whimsy_image_url   = ! empty( $instance[ 'whimsy_image_url' ] ) ? $instance[ 'whimsy_image_url' ] : '';
+            $whimsy_about_title = '';
+            if(isset($instance['whimsy_about_title']))
+            {
+                $whimsy_about_title = $instance['whimsy_about_title'];
+            }
+
             $whimsy_more        = ! empty( $instance[ 'whimsy_more' ] ) ? $instance[ 'whimsy_more' ] : '';
+
             $whimsy_more_link   = ! empty( $instance[ 'whimsy_more_link' ] ) ? $instance[ 'whimsy_more_link' ] : '';
+
             $whimsy_text        = ! empty( $instance[ 'whimsy_text' ] ) ? $instance[ 'whimsy_text' ] : '';
 
+            $whimsy_image = '';
+            if(isset($instance['whimsy_image']))
+            {
+                $whimsy_image = $instance['whimsy_image'];
+            }
         ?>
 
             <p>
-                <label for="<?php echo esc_attr( $this->get_field_id( 'whimsy_title' ) ); ?>"><?php esc_html_e( 'Title:', 'whimsy-framework' ); ?></label> 
-                <input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'whimsy_title' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'whimsy_title' ) ); ?>" type="text" value="<?php echo esc_attr( $whimsy_title ); ?>">
+                <label for="<?php echo esc_attr( $this->get_field_id( 'whimsy_about_title' ) ); ?>"><?php esc_html_e( 'Title:', 'whimsy-framework' ); ?></label> 
+                <input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'whimsy_about_title' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'whimsy_about_title' ) ); ?>" type="text" value="<?php echo esc_attr( $whimsy_about_title ); ?>">
             </p>
             <p>
-                <label for="<?php echo esc_attr( $this->get_field_id( 'whimsy_image_url' ) ); ?>"><?php esc_html_e( 'Image URL:', 'whimsy-framework' ); ?></label> 
-                <input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'whimsy_image_url' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'whimsy_image_url' ) ); ?>" type="text" value="<?php echo esc_attr( $whimsy_image_url ); ?>">
+                <label for="<?php echo esc_attr( $this->get_field_name( 'whimsy_image' ) ); ?>"><?php esc_html_e( 'Image:', 'whimsy-framework' ); ?></label>
+                <input name="<?php echo esc_attr( $this->get_field_name( 'whimsy_image' ) ); ?>" id="<?php echo esc_attr( $this->get_field_id( 'whimsy_image' ) ); ?>" class="widefat" type="text" size="36"  value="<?php echo esc_url( $whimsy_image ); ?>" />
+                <input class="upload_image_button" type="button" value="Upload Image" />
             </p>
             <p>
                 <label for="<?php echo esc_attr( $this->get_field_id( 'whimsy_text' ) ); ?>"><?php esc_html_e( 'Bio Blurb:', 'whimsy-framework' ); ?></label> 
@@ -89,8 +130,8 @@ class Widget_Whimsy_About extends WP_Widget {
     public function update( $new_instance, $old_instance ) {
 
         $instance = array();
-        $instance[ 'whimsy_title' ]     = ( ! empty( $new_instance[ 'whimsy_title' ] ) ) ? esc_attr( $new_instance[ 'whimsy_title' ] ) : '';
-        $instance[ 'whimsy_image_url' ] = ( ! empty( $new_instance[ 'whimsy_image_url' ] ) ) ? esc_attr( $new_instance[ 'whimsy_image_url' ] ) : '';
+        $instance[ 'whimsy_about_title' ]     = ( ! empty( $new_instance[ 'whimsy_about_title' ] ) ) ? esc_attr( $new_instance[ 'whimsy_about_title' ] ) : '';
+        $instance[ 'whimsy_image' ] = ( ! empty( $new_instance[ 'whimsy_image' ] ) ) ? esc_attr( $new_instance[ 'whimsy_image' ] ) : '';
         $instance[ 'whimsy_more' ]      = ( ! empty( $new_instance[ 'whimsy_more' ] ) ) ? esc_attr( $new_instance[ 'whimsy_more' ] ) : '';
         $instance[ 'whimsy_more_link' ] = ( ! empty( $new_instance[ 'whimsy_more_link' ] ) ) ? esc_attr( $new_instance[ 'whimsy_more_link' ] ) : '';
         $instance[ 'whimsy_text' ]      = ( ! empty( $new_instance[ 'whimsy_text' ] ) ) ? esc_textarea( $new_instance[ 'whimsy_text' ] ) : '';
