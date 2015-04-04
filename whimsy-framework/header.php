61fa6e47da9dc3043f1841a5af95e31c412e4fcc
natashakcross <natasha@Natashas-MacBook-Pro.local>
2015-04-03 20:56:40 -0500

    Remove site desc from mobile view

--- whimsy-framework/header.php
+++ whimsy-framework/header.php
@@ -45,12 +45,9 @@
 				        <a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><img src="<?php echo esc_url( get_theme_mod( 'whimsy_framework_logo_mobile' ) ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>"></a>
 				    </div>
 				
-				<?php else : // If no logo is set, display title and description text. ?>
+				<?php else : // If no logo is set, display title as text. ?>
 
-					<hgroup>
 						<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-						<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-					</hgroup>
 
 				<?php endif; // End mobile logo check. ?>
 
