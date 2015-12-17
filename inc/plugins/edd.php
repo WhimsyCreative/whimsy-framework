4b26f36a0e319c2bf8078277c427bc737228a68d
natashakcross <natasha@Natashas-MacBook-Pro.local>
2015-12-17 11:43:49 -0600

    1.3

---
+++ inc/plugins/edd.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Easy Digital Downloads Compatibility File
+ * See: http://easydigitaldownloads.com
+ *
+ * @package whimsy-framework
+ */
+
+/*
+ * Easy Digital Downloads Integration
+ */
+if ( class_exists( 'Easy_Digital_Downloads' ) ) {
+
+	function whimsy_edd_styles() {
+    	wp_enqueue_style( 'whimsy-edd-styles', get_template_directory_uri() . '/css/plugins/edd.css' );
+	}
+	
+	add_action( 'wp_enqueue_scripts', 'whimsy_edd_styles' );
+
+}
