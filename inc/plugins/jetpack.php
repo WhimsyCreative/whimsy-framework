61338f496388a27fc9a1e8832614e46d3f3ff85d
natashakcross <natasha@Natashas-MacBook-Pro.local>
2016-05-21 11:49:10 -0500

    2.0.0

--- inc/plugins/jetpack.php
+++ inc/plugins/jetpack.php
@@ -6,15 +6,32 @@
  * @package whimsy
  */
 
-/**
- * Add theme support for Infinite Scroll.
- * See: http://jetpack.me/support/infinite-scroll/
- */
 function whimsy_jetpack_setup() {
+	/**
+	 * Add theme support for Infinite Scroll.
+	 * See: https://jetpack.me/support/infinite-scroll/
+	 */
 	add_theme_support( 'infinite-scroll', array(
 		'type'      => 'click',
 		'container' => 'main',
+		'render'    => 'whimsy_infinite_scroll_render',
 		'footer'    => false,
 	) );
-}
+
+	/**
+	 * Add theme support for Responsive Videos.
+	 * See: https://jetpack.me/support/responsive-videos/
+	 */
+	add_theme_support( 'jetpack-responsive-videos' );
+} // end function whimsy_jetpack_setup
 add_action( 'after_setup_theme', 'whimsy_jetpack_setup' );
+
+/**
+ * Custom render function for Infinite Scroll.
+ */
+function whimsy_infinite_scroll_render() {
+	while ( have_posts() ) {
+		the_post();
+		get_template_part( 'template-parts/content', get_post_format() );
+	}
+} // end function whimsy_infinite_scroll_render
