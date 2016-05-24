eab74a5d499a807185ee83f38ccab36ecd6b419c
natashakcross <natasha@Natashas-MacBook-Pro.local>
2016-05-24 14:56:46 -0500

    Conditional check for jetpack class

--- inc/plugins/jetpack.php
+++ inc/plugins/jetpack.php
@@ -3,35 +3,59 @@
  * Jetpack Compatibility File
  * See: http://jetpack.me/
  *
- * @package whimsy
+ * @package whimsy-framework
  */
 
-function whimsy_jetpack_setup() {
-	/**
-	 * Add theme support for Infinite Scroll.
-	 * See: https://jetpack.me/support/infinite-scroll/
-	 */
-	add_theme_support( 'infinite-scroll', array(
-		'type'      => 'click',
-		'container' => 'main',
-		'render'    => 'whimsy_infinite_scroll_render',
-		'footer'    => false,
-	) );
+if ( class_exists( 'Jetpack' ) ) {
 
-	/**
-	 * Add theme support for Responsive Videos.
-	 * See: https://jetpack.me/support/responsive-videos/
-	 */
-	add_theme_support( 'jetpack-responsive-videos' );
-} // end function whimsy_jetpack_setup
-add_action( 'after_setup_theme', 'whimsy_jetpack_setup' );
+    if ( ! function_exists( 'whimsy_jetpack_styles' ) ) :
 
-/**
- * Custom render function for Infinite Scroll.
- */
-function whimsy_infinite_scroll_render() {
-	while ( have_posts() ) {
-		the_post();
-		get_template_part( 'template-parts/content', get_post_format() );
-	}
-} // end function whimsy_infinite_scroll_render
+        function whimsy_jetpack_styles() {
+            wp_enqueue_style( 'whimsy-jetpack-styles', get_template_directory_uri() . '/css/plugins/jetpack.css' );
+        }
+
+        add_action( 'wp_enqueue_scripts', 'whimsy_jetpack_styles' );
+    
+    endif;
+    
+    if ( ! function_exists( 'whimsy_jetpack_setup' ) ) :
+
+        function whimsy_jetpack_setup() {
+            /**
+             * Add theme support for Infinite Scroll.
+             * See: https://jetpack.me/support/infinite-scroll/
+             */
+            add_theme_support( 'infinite-scroll', array(
+                'type'      => 'click',
+                'container' => 'main',
+                'render'    => 'whimsy_infinite_scroll_render',
+                'footer'    => false,
+            ) );
+
+            /**
+             * Add theme support for Responsive Videos.
+             * See: https://jetpack.me/support/responsive-videos/
+             */
+            add_theme_support( 'jetpack-responsive-videos' );
+
+        } // end function whimsy_jetpack_setup
+        add_action( 'after_setup_theme', 'whimsy_jetpack_setup' );
+
+    endif;
+
+    /**
+     * Custom render function for Infinite Scroll.
+     */
+    
+    if ( ! function_exists( 'whimsy_infinite_scroll_render' ) ) :
+
+        function whimsy_infinite_scroll_render() {
+            while ( have_posts() ) {
+                the_post();
+                get_template_part( 'template-parts/content', get_post_format() );
+            }
+        } // end function whimsy_infinite_scroll_render
+
+    endif;
+    
+}
