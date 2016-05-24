81952628ca864c2fa5cd76d8dd0b35e27184777e
natashakcross <natasha@Natashas-MacBook-Pro.local>
2016-05-24 14:54:01 -0500

    Formatting changes

--- content-link.php
+++ content-link.php
@@ -12,8 +12,9 @@
 
 	<div class="entry-meta">
 
-	<!-- Link Post Format: Add a title and place the URL by itself in content editor -->
-	<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_the_content() ) ), '</a></h1>' ); ?>
+	   <!-- Link Post Format: Add a title and place the URL by itself in content editor -->
+	   
+        <?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_the_content() ) ), '</a></h1>' ); ?>
 	
 	<?php whimsy_post_meta_after(); ?>
 
--- content-page.php
+++ content-page.php
@@ -11,7 +11,9 @@
 	<?php whimsy_post_meta_before(); ?>
 
 	<div class="entry-meta">
+        
 		<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
+        
 	</div>
 	
 	<?php whimsy_post_meta_after(); ?>
@@ -19,13 +21,16 @@
 	</header><!-- .entry-header -->
 	
 	<div class="entry-content">
+        
 		<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'whimsy-framework' ) ); ?>
+        
 		<?php
 			wp_link_pages( array(
 				'before' => '<div class="page-links">' . __( 'Pages:', 'whimsy-framework' ),
 				'after'  => '</div>',
 			) );
 		?>
+        
 	</div><!-- .entry-content -->
 
 </article><!-- #post-## -->
