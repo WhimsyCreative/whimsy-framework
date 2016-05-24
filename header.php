a88c92bf01c82ba7a555825d307964876358038e
natashakcross <natasha@Natashas-MacBook-Pro.local>
2016-05-24 14:56:56 -0500

    Fixed function prefix

--- header.php
+++ header.php
@@ -12,7 +12,7 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <?php
-	if ( ! function_exists( '_wp_render_title_tag' ) ) {
+	if ( ! function_exists( 'whimsy_render_title_tag' ) ) {
 		function whimsy_render_title() {
 	?>
 	<title><?php wp_title( '|', true, 'right' ); ?></title>
