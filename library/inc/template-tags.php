7b84d7fc9fe2cdba0f2cfdaaf7f274c329f43c45
Natasha K. Cross <natasha@thefanciful.com>
2017-01-17 08:32:32 -0600

    Changed load order for post meta

--- library/inc/template-tags.php
+++ library/inc/template-tags.php
@@ -5,16 +5,17 @@
  * @package whimsy-framework
  */
 
-add_action( 'whimsy_post_meta_before',  'whimsy_display_post_categories', 10 );
-add_action( 'whimsy_post_meta_before',  'whimsy_display_post_thumbnail', 10);
-add_action( 'whimsy_post_meta_after',   'whimsy_display_post_meta', 20 );
 add_action( 'whimsy_nav_inside_before', 'whimsy_responsive_nav', 40 );
 
+add_action( 'whimsy_post_meta_before',  'whimsy_display_post_thumbnail', 10);
+add_action( 'whimsy_post_meta_after',  'whimsy_display_post_categories', 10 );
+add_action( 'whimsy_post_meta_after',   'whimsy_display_post_meta', 20 );
 //add_action( 'whimsy_post_meta_after', 'whimsy_author_gravatar', 15 );
 
 /**
  * Custom functions that act independently of the theme templates.
  */
+require_once get_template_directory() . '/library/inc/functions/responsive-nav.php';
 require_once get_template_directory() . '/library/inc/functions/author-gravatar.php';
 require_once get_template_directory() . '/library/inc/functions/categorized-blog.php';
 require_once get_template_directory() . '/library/inc/functions/display-post-categories.php';
@@ -23,4 +24,3 @@ require_once get_template_directory() . '/library/inc/functions/display-post-met
 require_once get_template_directory() . '/library/inc/functions/paging-nav.php';
 require_once get_template_directory() . '/library/inc/functions/post-nav.php';
 require_once get_template_directory() . '/library/inc/functions/posted-on.php';
-require_once get_template_directory() . '/library/inc/functions/responsive-nav.php';
