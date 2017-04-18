3f05f80c0a902d9b8a2a48215e7e7a551fdcde78
natashacozad <hello@whimsycreative.co>
2017-04-18 11:28:50 -0500

    2.1

--- content-video.php
+++ content-video.php
@@ -7,7 +7,6 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
 	<div class="entry-content">
-        <?php echo ( $video = hybrid_media_grabber( array( 'type' => 'video', 'split_media' => true ) ) ); ?>
 
 		<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'whimsy-framework' ) ); ?>
 		<?php
